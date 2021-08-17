a122fe18cc0ed5b95724c57d463b13804fd9f5c7
montblanc18 <skurita.phys@gmail.com>
2021-08-18 02:00:16 +0900

    update conv

--- client/omega2plus/go.mod
+++ client/omega2plus/go.mod
@@ -7,7 +7,7 @@ require (
 	github.com/go-openapi/errors v0.20.0 // indirect
 	github.com/go-openapi/runtime v0.19.24
 	github.com/go-openapi/strfmt v0.20.1 // indirect
-	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-openapi/swag v0.19.15
 	github.com/go-openapi/validate v0.20.2 // indirect
 	github.com/montblanc18/gps-viewer v0.0.0-20210815151403-cbe0c314d5f5
 	github.com/rs/zerolog v1.23.0
--- client/omega2plus/model.go
+++ client/omega2plus/model.go
@@ -2,7 +2,10 @@ package omega2plus
 
 import (
 	"context"
+	"strconv"
 
+	"github.com/go-openapi/strfmt"
+	"github.com/go-openapi/swag"
 	"github.com/montblanc18/gps-viewer/client/gen/models"
 )
 
@@ -18,8 +21,29 @@ type Payloads struct {
 	Course     string `json:"course"`
 	Speed      string `json:"speed"`
 	RecordedAt string `json:"recorded_at"`
+	DeviceID   string `json:"device_id"`
 }
 
-func (u Uplink) conv(ctx context.Context) *models.RegisterDeviceGPS {
-	return &models.RegisterDeviceGPS{}
+func (u Uplink) conv(ctx context.Context) (*models.RegisterDeviceGPS, error) {
+	lng, err := strconv.ParseFloat(u.Payloads.Longitude, 64)
+	if err != nil {
+		return &models.RegisterDeviceGPS{}, err
+	}
+
+	lat, err := strconv.ParseFloat(u.Payloads.Latitude, 64)
+	if err != nil {
+		return &models.RegisterDeviceGPS{}, err
+	}
+	recordedAt, err := strfmt.ParseDateTime(u.Payloads.RecordedAt)
+	if err != nil {
+		return &models.RegisterDeviceGPS{}, err
+	}
+	deviceType := "omega2plus"
+	return &models.RegisterDeviceGPS{
+		Lng:        swag.Float64(lng),
+		Lat:        swag.Float64(lat),
+		DeviceID:   &u.Payloads.DeviceID,
+		DeviceType: &deviceType,
+		RecordedAt: &recordedAt,
+	}, nil
 }
