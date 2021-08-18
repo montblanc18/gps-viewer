6e3787802e02cf388b9b69d010c835bfb1472517
montblanc18 <skurita.phys@gmail.com>
2021-08-18 23:27:10 +0900

    testutilsに切り出し

---
+++ server/testutils.go
@@ -0,0 +1,19 @@
+package server
+
+import (
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+func cmdExec(t *testing.T, cmds []string) {
+	t.Helper()
+	for _, cmd := range cmds {
+		t.Logf("[INFO] %s", cmd)
+		args := strings.Split(cmd, " ")
+
+		if err := exec.Command(args[0], args[1:]...).Run(); err != nil {
+			t.Logf("[WARN] %s %v", cmd, err)
+		}
+	}
+}
