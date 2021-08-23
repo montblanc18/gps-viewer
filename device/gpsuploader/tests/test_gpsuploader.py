9987f89ac54e40bcd0c3c68505dfea9ca68efce7
montblanc18 <skurita.phys@gmail.com>
2021-08-23 22:46:00 +0900

    add test

--- device/gpsuploader/tests/test_gpsuploader.py
+++ device/gpsuploader/tests/test_gpsuploader.py
@@ -1,14 +1,31 @@
+import copy
 import json
 import os
 
 import gpsuploader.gpsuploader as gpsu
 
 
-def test_gpsuploader():
+def test_getgps():
     json_data = r"{\"age\":0, \"latitude\":\"43.8616\", \"longitude\":\"-79.3854\", \"elevation\":\"184.0\", \"course\":\"\", \"speed\":\"N\"}"
     want_json_data = '{"age":0, "latitude":"43.8616", "longitude":"-79.3854", "elevation":"184.0", "course":"", "speed":"N"}'
     cmd = "echo {0}".format(json_data)
     assert gpsu.get_gps(cmd) == json.loads(json.dumps(want_json_data))
+
+
+def test_add_record():
+    key = "add_key"
+    value = "add_value"
+    d = {
+        "age": 0,
+        "latitude": "43.8616",
+        "longitude": "-79.3854",
+        "elevation": "184.0",
+        "course": "",
+        "speed": "N",
+    }
+    want_d = copy.copy(d)
+    want_d[key] = value
+    assert gpsu.add_record(d, key, value) == want_d
 
 
 def test_put_item():
